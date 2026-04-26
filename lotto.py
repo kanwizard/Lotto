b083cfbe91990e8166ba314026b04e1581208445
kanwizard <kanwizard@nate.com>
2026-04-26 23:14:43 +0900

    lotto.py

--- lotto.py
+++ lotto.py
@@ -1,24 +1,6 @@
 import streamlit as st
 import random
 
-
-# 로그인 기능 추가
-def login():
-    st.title("🔐 로그인")
-    password = st.text_input("비밀번호를 입력하세요", type="password")
-    
-    # 비밀번호 확인 버튼
-    if st.button("확인"):
-        if password == "860716":  # 원하는 비밀번호로 변경 가능
-            st.success("로그인 성공!")
-            st.session_state.logged_in = True  # 로그인 상태 저장
-            return True
-        else:
-            st.error("비밀번호가 틀렸습니다.")
-            return False
-    return False  # 로그인 상태가 아닐 경우 계속 대기
-
-
 st.set_page_config(page_title="로또 생성기", page_icon="🎰")
 
 st.title("🎰 로또 생성기 (연속번호 제한)")
